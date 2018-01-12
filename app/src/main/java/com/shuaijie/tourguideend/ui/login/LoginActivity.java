61b3b016ac20f642523285b718e988708d6f175b
shuaiJie <13838469758@qq.com>
2018-01-12 16:42:51 +0800

    d

--- app/src/main/java/com/shuaijie/tourguideend/ui/login/LoginActivity.java
+++ app/src/main/java/com/shuaijie/tourguideend/ui/login/LoginActivity.java
@@ -14,7 +14,7 @@ import com.shuaijie.tourguideend.ui.main.MainActivity;
 import com.shuaijie.tourguideend.ui.register.RegisterActivity;
 
 /**
- * 登录对方的世界观
+ * 登录
  */
 public class LoginActivity extends BaseActivity implements View.OnClickListener {
     private SharedPreferences sp;
