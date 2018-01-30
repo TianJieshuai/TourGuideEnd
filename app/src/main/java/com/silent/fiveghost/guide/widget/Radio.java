0ed7c2aceaf4fb383d2bb1ae45d4f56affb13437
shuaiJie <13838469758@qq.com>
2018-01-30 19:18:23 +0800

    修改登录页面在不同分辨率下出现白条

--- app/src/main/java/com/silent/fiveghost/guide/widget/Radio.java
+++ app/src/main/java/com/silent/fiveghost/guide/widget/Radio.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
@@ -41,7 +43,13 @@ import android.util.AttributeSet;
 public class Radio extends android.support.v7.widget.AppCompatRadioButton {
 
     private Bitmap bitmap;
-    private int bitmapWidth, bitmapBeight;
+    private int bitmapWidth, bitmapHeight;
+    private int measuredWidth;
+    private int measuredHeight;
+    private Paint paint;
+    private Point p1, p2, p3, p4, p5, p6;
+    private Rect bitmapRect;
+    private Path mPath;
 
     public Radio(Context context) {
         this(context, null, 0);
@@ -59,29 +67,65 @@ public class Radio extends android.support.v7.widget.AppCompatRadioButton {
         if (drawable != null)
             if (drawable instanceof BitmapDrawable) {
                 Bitmap bm = ((BitmapDrawable) drawable).getBitmap();
-                bitmap = Bitmap.createBitmap(bm.getWidth(), bm.getHeight(), Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(bitmap);
-
-                Paint paint = new Paint();
-
-                final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-                final RectF rectF = new RectF(rect);
-                paint.setAntiAlias(true);
-                canvas.drawARGB(0, 0, 0, 0);
-                paint.setColor(0xff424242);
-
-                canvas.drawRoundRect(rectF, 50, 50, paint);
-                paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-                canvas.drawBitmap(bm, rect, rect, paint);
+                setImageBitmap(bm);
+                init();
             }
+    }
+
+    public void init() {
+        measuredWidth = getMeasuredWidth();
+        measuredHeight = getMeasuredHeight();
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(0xff424242);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+        // 开始点
+        p1 = new Point(0, 0);
+        // 圆左边线
+        p2 = new Point(measuredWidth / 2 - bitmapWidth, 0);
+        // 圆右边线
+        p3 = new Point(measuredWidth / 2 + bitmapWidth, 0);
+        // 右点
+        p4 = new Point(measuredWidth, 0);
+        // 右下点
+        p5 = new Point(measuredWidth, measuredHeight);
+        // 左下点
+        p6 = new Point(0, measuredHeight);
+        // 画圆区域
+        bitmapRect = new Rect(p2.x, p2.y - bitmapHeight, p3.x, p3.y + bitmapHeight);
+        mPath = new Path();
+        // 重绘
+        invalidate();
+    }
+
+    public void setImageBitmap(Bitmap bm) {
+        bitmap = Bitmap.createBitmap(bm.getWidth(), bm.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        canvas.drawARGB(0, 0, 0, 0);
+
+        canvas.drawRoundRect(rectF, 50, 50, paint);
+        canvas.drawBitmap(bm, rect, rect, paint);
+        bitmapWidth = bitmap.getWidth() / 2;
+        bitmapHeight = bitmap.getHeight() / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (bitmap != null) {
-            bitmapWidth = bitmap.getWidth() / 2;
-            bitmapBeight = bitmap.getHeight() / 2;
+            mPath.moveTo(p1.x, p1.y);
+            mPath.lineTo(p2.x, p2.y);
+            mPath.arcTo(new RectF(bitmapRect), 180, 180);
+            mPath.lineTo(p4.x, p4.y);
+            mPath.lineTo(p5.x, p5.y);
+            mPath.lineTo(p6.x, p6.y);
+            mPath.lineTo(p1.x, p1.y);
+            canvas.drawPath(mPath, paint);
+            canvas.drawRoundRect(new RectF(bitmapRect), 0, 0, paint);
         }
     }
 }
