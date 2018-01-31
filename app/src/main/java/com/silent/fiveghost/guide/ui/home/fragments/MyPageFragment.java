1c4e237893bb3cd2334de7654ba35f68336df55f
习爸爸 <837589611@qq.com>
2018-01-31 17:24:49 +0800

    完成..

--- app/src/main/java/com/silent/fiveghost/guide/ui/home/fragments/MyPageFragment.java
+++ app/src/main/java/com/silent/fiveghost/guide/ui/home/fragments/MyPageFragment.java
@@ -4,12 +4,16 @@ package com.silent.fiveghost.guide.ui.home.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
+import android.widget.Toast;
 
 import com.silent.fiveghost.guide.R;
 import com.silent.fiveghost.guide.base.fragment.BaseFragment;
 import com.silent.fiveghost.guide.ui.home.MyRouteActivity;
 import com.silent.fiveghost.guide.ui.home.MySettingActivity;
+import com.silent.fiveghost.guide.ui.home.adapters.PersonalAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +22,7 @@ public class MyPageFragment extends BaseFragment implements View.OnClickListener
 
     private com.zhy.autolayout.AutoRelativeLayout mSetting;
     private com.zhy.autolayout.AutoRelativeLayout mRoute;
+    private RecyclerView mPersion_recyc;
 
     @Override
     protected void initData(Bundle arguments) {
@@ -33,7 +38,18 @@ public class MyPageFragment extends BaseFragment implements View.OnClickListener
     protected void initView() {
         mSetting  =  (com.zhy.autolayout.AutoRelativeLayout)findViewById(R.id.mSetting);
         mRoute  = (com.zhy.autolayout.AutoRelativeLayout)findViewById(R.id.mRoute);
+        mPersion_recyc =(RecyclerView) findViewById(R.id.mPersion_recyc);
         mRoute.setOnClickListener(this);
+
+        mPersion_recyc.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL));
+        PersonalAdapter adapter = new PersonalAdapter(getActivity());
+        mPersion_recyc.setAdapter(adapter);
+        adapter.setOnClickListener(new PersonalAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
