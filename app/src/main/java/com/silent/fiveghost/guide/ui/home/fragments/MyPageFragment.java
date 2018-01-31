c76d205b1b26da332e3015b0c35bf7f3bc5c8011
1924409769 <1924409769@qq.com>
2018-01-31 08:53:47 +0800

    订单详情

--- app/src/main/java/com/silent/fiveghost/guide/ui/home/fragments/MyPageFragment.java
+++ app/src/main/java/com/silent/fiveghost/guide/ui/home/fragments/MyPageFragment.java
@@ -3,32 +3,37 @@ package com.silent.fiveghost.guide.ui.home.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.silent.fiveghost.guide.R;
 import com.silent.fiveghost.guide.base.fragment.BaseFragment;
+import com.silent.fiveghost.guide.utils.CircularImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyPageFragment extends BaseFragment {
 
-
-    public MyPageFragment() {
-        // Required empty public constructor
-    }
-
+    private CircularImageView user_icon;
+    private TextView user_name;
+    private TextView user_sex;
+    private RecyclerView mPersion_recyc;
+    private TextView my_route;
+    private TextView my_SafetySetting;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false);
+        View view = View.inflate(getActivity(), R.layout.fragment_my_page, null);
+        initView(view);
+        return view;
     }
-
-    @Override
+        @Override
     protected void initData(Bundle arguments) {
 
     }
@@ -37,5 +42,16 @@ public class MyPageFragment extends BaseFragment {
     protected void run() {
 
     }
+
+    private void initView(View view) {
+        user_icon = (CircularImageView) view.findViewById(R.id.user_icon);
+        user_name = (TextView) view.findViewById(R.id.user_name);
+        user_sex = (TextView) view.findViewById(R.id.user_sex);
+        mPersion_recyc = (RecyclerView) view.findViewById(R.id.mPersion_recyc);
+        my_route = (TextView) view.findViewById(R.id.my_route);
+        my_SafetySetting = (TextView) view.findViewById(R.id.my_SafetySetting);
+    }
+
+
 
 }
